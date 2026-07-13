a0d3a3e186011f2e6c6d3de3b6fdbe461bb283c8
Gladiador26 <pocholocalvo2043@gmail.com>
2026-07-13 17:39:40 -0400

    Update APPS_SCRIPT_URL to new endpoint

--- index.py
+++ index.py
@@ -11,7 +11,7 @@ WHATSAPP_TOKEN = "EAAS11GIEA50BRvJRK4ZBCedOYRy8dfLlEgYc3GoZCT7nigtxPuy7ED5SR5oEA
 PHONE_NUMBER_ID = "1253869841136312"
 
 # 👇 PEGA TU NUEVA URL DE APPS SCRIPT AQUÍ 👇
-APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwEezo88-bTmu-_RIk12zY56cBgiG-KfhR8K-BuqRhBYb92wVaDssDYz_V76sTccz78cA/exec" 
+APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxAULTbGWuBXIhOQKms3CsDsInyDB8IbVXGv31gCgdrv2rfKZVndZ5VcwmEoJK_csvJ3A/exec" 
 
 PERFILES = {
     "59178150540": "admin",             
